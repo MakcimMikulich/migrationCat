6cb91757e2825a0dfead882730c681143203bcf3
Makcik303123 <adoberoflit@gmail.com>
2024-12-18 08:48:09 +0300

    add config middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,12 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
 // Создаем middleware с учетом конфигурации маршрутов
-export default createMiddleware(routing);
+export default createMiddleware({
+    ...routing,
+    localeDetection: false, // Отключаем автоматическое определение по заголовкам и cookies
+});
 
 export const config = {
-	// Подключаем middleware для корневого маршрута и всех локалей
-	matcher: ["/", "/(en|ru|china)/:path*"],
+    // Подключаем middleware для корневого маршрута и всех локалей
+    matcher: ["/", "/(en|ru|china)/:path*"],
 };
